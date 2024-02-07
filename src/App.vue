7726effe65231110ec10aa3832fc8845c9678636
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-07 17:13:41 +0100

    concluso "content goes here

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
 
+
     <HeaderSec/>
     <MainContent/>
     <FooterSec/>
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -14,7 +14,23 @@
 
 <template>
 
+    <div class="back">
+        <div class="container p-5">
+            <h2 class="text-uppercase"> --> content goes here &lt;-- </h2>
+        </div>
+    </div>
+    <CtaUser/>
+
 </template>
 
-<style>
+<style scoped>
+
+div.back{
+    background-color: #1c1c1c;
+}
+
+h2{
+    color: white;
+}
+
 </style>
