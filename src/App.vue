2f45bfa28b641864d388817f6395632b23ff9ad7
AlessandroGCatino <catino.alessandrog@gmail.com>
2024-02-07 17:39:03 +0100

    concluso e integrato ctaUser (bonus)

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,15 @@
 <style lang="scss">
 
 @use "./styles/general.scss";
+@import url('https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+
 
 *{
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: "PT Sans Narrow", sans-serif;
+    font-weight: 900;
 }
 
 figure{
--- src/components/CtaUser.vue
+++ src/components/CtaUser.vue
@@ -1,12 +1,61 @@
 <script>
     export default{
-        name: "CtaUser"
+        name: "CtaUser",
+        data(){
+            return{
+                ctaElements: [
+                    {
+                        icon: "src/assets/img/buy-comics-digital-comics.png",
+                        text: "digital comics"
+                    },
+                    {
+                        icon: "src/assets/img/buy-comics-merchandise.png",
+                        text: "dc merchandise"
+                    },
+                    {
+                        icon: "src/assets/img/buy-comics-subscriptions.png",
+                        text: "subscription"
+                    },
+                    {
+                        icon: "src/assets/img/buy-comics-shop-locator.png",
+                        text: "comic shop locator"
+                    },
+                    {
+                        icon: "src/assets/img/buy-dc-power-visa.svg",
+                        text: "dc power visa"
+                    },
+                ]
+            }
+        }
     }
 </script>
 
 <template>
+    <div id="ctaBack">
+        <div class="container">
+            <ul class="d-flex gap-5 align-items-center justify-content-center py-4">
+                <li v-for="(element, index) in ctaElements" :key="index"
+                class="d-flex align-items-center gap-2 p-2">
+                    <figure>
+                        <img :src="element.icon" alt="" width="45">
+                    </figure>
+                    <span class="text-uppercase">
+                        {{ element.text }}
+                    </span>
+
+                </li>
+            </ul>
+        </div>
+    </div>
 
 </template>
 
-<style>
+<style scoped>
+
+#ctaBack{
+    background-color: #0282f9;
+    color: white;
+    font-weight: 500;
+}
+
 </style>
